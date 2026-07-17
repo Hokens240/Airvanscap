562cb591e086a7cf6bf366def8038e1480285bc2
peakcap <peaktiercapital@gmail.com>
2026-07-17 14:59:48 -0700

    Update myapp.js

--- js/myapp.js
+++ js/myapp.js
@@ -98,10 +98,10 @@ function initializeMockUsers() {
             country: "United States of America", 
             pass: null, 
             accountBalance: "1,914.00", 
-            totalProfit: "26815.00", 
-            profitBalance: "738.00", 
+            totalProfit: "27195.00", 
+            profitBalance: "995.00", 
             initialInvestment: "1,500.00", 
-            returnOnInvestment: "25854.00",
+            returnOnInvestment: "26231.00",
             investments: []
         },
     ];
